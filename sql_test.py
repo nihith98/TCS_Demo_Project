783ad6c7c56b65d9c810730cafcf136aa8865855
Nihith Sistla <nihithsistla98@gmail.com>
2020-09-07 12:58:34 +0530

    Added the write module

--- sql_test.py
+++ sql_test.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from sql_write import *
 
 #Connection with mySQL
 mydb = mysql.connector.connect(host='127.0.0.1',user='root',password='Nihith@98',database='Tariff_Module')
@@ -21,3 +22,21 @@ print("Showing all the tables:")
 mycursor.execute('SHOW TABLES')
 for x in mycursor:
     print(x)
+
+
+#mycursor.execute('ALTER TABLE plans ADD name VARCHAR(30)')
+#mycursor.execute("ALTER TABLE plans ADD type CHAR(1)")
+#mycursor.execute('ALTER TABLE plans ADD tariff FLOAT(4)')
+#mycursor.execute('ALTER TABLE plans ADD validity INT')
+#mycursor.execute('ALTER TABLE plans ADD rental VARCHAR(100)')
+mycursor.execute("SELECT * FROM plans")
+
+print(type(mycursor.description[1]))
+for x in mycursor.description:
+    print(x[0])
+
+#add_plan('voice3',0.6,20,'NA','V',1)
+#mycursor.execute("ALTER TABLE plans ADD enable_flag INT")
+#disable_plan(4)
+enable_plan(4)
+delete_plan(5)
---
+++ sql_write.py
@@ -0,0 +1,43 @@
+import mysql.connector
+
+def add_plan(add_name,add_tariff,add_validity,add_rental,add_type,enable_flag=1):
+    mydb = mysql.connector.connect(host='127.0.0.1',user='root',password='Nihith@98',database='Tariff_Module')
+    add_cursor = mydb.cursor()
+    strcmd = "INSERT INTO plans (name, tariff, validity, rental,type,enable_flag) VALUES ('"+add_name+"',"+str(add_tariff)+','+str(add_validity)+",'"+add_rental+"','"+add_type+"',"+str(enable_flag)+")" 
+    print(strcmd)
+    add_cursor.execute(strcmd)
+    mydb.commit()
+    add_cursor.close()
+
+def update_plan(up_id,up_name,up_tariff,up_validity,up_rental,up_type,enable_flag=1):
+    mydb = mysql.connector.connect(host='127.0.0.1',user='root',password='Nihith@98',database='Tariff_Module')
+    update_cursor = mydb.cursor()
+    strcmd = "UPDATE plans SET name='"+up_name+"',tariff="+str(up_tariff)+",validity="+str(up_validity)+",rental='"+up_rental+"',type='"+up_type+"',enable_flag="+str(enable_flag)+" WHERE plan_id="+str(up_id)
+    print(strcmd)
+    update_cursor.execute(strcmd)
+    mydb.commit()
+    update_cursor.close()
+
+def disable_plan(up_id):
+    mydb = mysql.connector.connect(host='127.0.0.1',user='root',password='Nihith@98',database='Tariff_Module')
+    disable_cursor = mydb.cursor()
+    strcmd = "UPDATE plans SET enable_flag="+str(0)+" WHERE plan_id="+str(up_id)
+    disable_cursor.execute(strcmd)
+    mydb.commit()
+    disable_cursor.close()
+
+def enable_plan(up_id):
+    mydb = mysql.connector.connect(host='127.0.0.1',user='root',password='Nihith@98',database='Tariff_Module')
+    enable_cursor = mydb.cursor()
+    strcmd = "UPDATE plans SET enable_flag="+str(1)+" WHERE plan_id="+str(up_id)
+    enable_cursor.execute(strcmd)
+    mydb.commit()
+    enable_cursor.close()
+
+def delete_plan(up_id):
+    mydb = mysql.connector.connect(host='127.0.0.1',user='root',password='Nihith@98',database='Tariff_Module')
+    del_cursor = mydb.cursor()
+    strcmd = "DELETE FROM plans WHERE plan_id="+str(up_id)
+    del_cursor.execute(strcmd)
+    mydb.commit()
+    del_cursor.close()

7a9605bf6127cfdea2949d6674b65dd27c5e6403
Nihith Sistla <nihithsistla98@gmail.com>
2020-09-08 16:38:00 +0530

    template changed to templates to remove internal server error

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__,template_folder="template")
+app = Flask(__name__,template_folder="templates")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SQLALCHEMY_DATABASE_URI']="mysql+pymysql://root:Nihith@98@127.0.0.1:3306/Tariff_Module"
 db = SQLAlchemy(app)
